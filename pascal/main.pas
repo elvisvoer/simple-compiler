3a8b5ccc5017e8211164da686aabaae0ce722ba0
Elvis <elvis.adomnica@gmail.com>
2022-12-10 17:22:55 +0100

    Add support for single-character control statements

--- pascal/main.pas
+++ pascal/main.pas
@@ -1,17 +1,20 @@
 {--------------------------------------------------------------}
-program Cradle;
+program Branch;
 
 {--------------------------------------------------------------}
 { Constant Declarations }
 
 const TAB = ^I;
-CR = LineEnding;
+      CR  = ^M;
+
 
 {--------------------------------------------------------------}
 { Variable Declarations }
 
-var Look: char;              { Lookahead Character }
-                              
+var Look  : char;              { Lookahead Character }
+    Lcount: integer;           { Label Counter }
+
+
 {--------------------------------------------------------------}
 { Read New Character From Input Stream }
 
@@ -19,6 +22,7 @@ procedure GetChar;
 begin
    Read(Look);
 end;
+
 
 {--------------------------------------------------------------}
 { Report an Error }
@@ -57,18 +61,15 @@ begin
    else Expected('''' + x + '''');
 end;
 
-
 {--------------------------------------------------------------}
 { Recognize an Alpha Character }
 
 function IsAlpha(c: char): boolean;
 begin
-   IsAlpha := upcase(c) in ['A'..'Z'];
+   IsAlpha := UpCase(c) in ['A'..'Z'];
 end;
-                              
 
 {--------------------------------------------------------------}
-
 { Recognize a Decimal Digit }
 
 function IsDigit(c: char): boolean;
@@ -83,8 +84,23 @@ function IsAddop(c: char): boolean;
 begin
    IsAddop := c in ['+', '-'];
 end;
-{--------------------------------------------------------------}
 
+{--------------------------------------------------------------}
+{ Recognize White Space }
+
+function IsWhite(c: char): boolean;
+begin
+   IsWhite := c in [' ', TAB];
+end;
+
+{--------------------------------------------------------------}
+{ Skip Over Leading White Space }
+
+procedure SkipWhite;
+begin
+   while IsWhite(Look) do
+      GetChar;
+end;
 
 {--------------------------------------------------------------}
 { Get an Identifier }
@@ -96,7 +112,6 @@ begin
    GetChar;
 end;
 
-
 {--------------------------------------------------------------}
 { Get a Number }
 
@@ -107,6 +122,24 @@ begin
    GetChar;
 end;
 
+{--------------------------------------------------------------}
+{ Generate a Unique Label }
+
+function NewLabel: string;
+var S: string;
+begin
+   Str(LCount, S);
+   NewLabel := 'L' + S;
+   Inc(LCount);
+end;
+
+{--------------------------------------------------------------}
+{ Post a Label To Output }
+
+procedure PostLabel(L: string);
+begin
+   WriteLn(L, ':');
+end;
 
 {--------------------------------------------------------------}
 { Output a String with Tab }
@@ -126,137 +159,208 @@ begin
 end;
 
 {--------------------------------------------------------------}
+{ Parse and Translate a Boolean Condition }
+
+procedure Condition;
+begin
+   EmitLn('<condition>');
+end;
+
+{--------------------------------------------------------------}
+{ Parse and Translate a Math Expression }
+
+procedure Expression;
+begin
+   EmitLn('<expr>');
+end;
+
+{--------------------------------------------------------------}
+{ Recognize and Translate an IF Construct }
+
+procedure Block(L: string); Forward;
+
+
+procedure DoIf(L: string);
+var L1, L2: string;
+begin
+   Match('i');
+   Condition;
+   L1 := NewLabel;
+   L2 := L1;
+   EmitLn('BEQ ' + L1);
+   Block(L);
+   if Look = 'l' then begin
+      Match('l');
+      L2 := NewLabel;
+      EmitLn('BRA ' + L2);
+      PostLabel(L1);
+      Block(L);
+   end;
+   Match('e');
+   PostLabel(L2);
+end;
+
+
+{--------------------------------------------------------------}
+{ Parse and Translate a WHILE Statement }
+
+procedure DoWhile;
+var L1, L2: string;
+begin
+   Match('w');
+   L1 := NewLabel;
+   L2 := NewLabel;
+   PostLabel(L1);
+   Condition;
+   EmitLn('BEQ ' + L2);
+   Block(L2);
+   Match('e');
+   EmitLn('BRA ' + L1);
+   PostLabel(L2);
+end;
+
+
+{--------------------------------------------------------------}
+{ Parse and Translate a LOOP Statement }
+
+procedure DoLoop;
+var L1, L2: string;
+begin
+   Match('p');
+   L1 := NewLabel;
+   L2 := NewLabel;
+   PostLabel(L1);
+   Block(L2);
+   Match('e');
+   EmitLn('BRA ' + L1);
+   PostLabel(L2);
+end;
+
+
+{--------------------------------------------------------------}
+{ Parse and Translate a REPEAT Statement }
+
+procedure DoRepeat;
+var L1, L2: string;
+begin
+   Match('r');
+   L1 := NewLabel;
+   L2 := NewLabel;
+   PostLabel(L1);
+   Block(L2);
+   Match('u');
+   Condition;
+   EmitLn('BEQ ' + L1);
+   PostLabel(L2);
+end;
+
+
+{--------------------------------------------------------------}
+{ Parse and Translate a FOR Statement }
+
+procedure DoFor;
+var L1, L2: string;
+    Name: char;
+begin
+   Match('f');
+   L1 := NewLabel;
+   L2 := NewLabel;
+   Name := GetName;
+   Match('=');
+   Expression;
+   EmitLn('SUBQ #1,D0');
+   EmitLn('LEA ' + Name + '(PC),A0');
+   EmitLn('MOVE D0,(A0)');
+   Expression;
+   EmitLn('MOVE D0,-(SP)');
+   PostLabel(L1);
+   EmitLn('LEA ' + Name + '(PC),A0');
+   EmitLn('MOVE (A0),D0');
+   EmitLn('ADDQ #1,D0');
+   EmitLn('MOVE D0,(A0)');
+   EmitLn('CMP (SP),D0');
+   EmitLn('BGT ' + L2);
+   Block(L2);
+   Match('e');
+   EmitLn('BRA ' + L1);
+   PostLabel(L2);
+   EmitLn('ADDQ #2,SP');
+end;
+
+{--------------------------------------------------------------}
+{ Parse and Translate a DO Statement }
+
+procedure Dodo;
+var L1, L2: string;
+begin
+   Match('d');
+   L1 := NewLabel;
+   L2 := NewLabel;
+   Expression;
+   EmitLn('SUBQ #1,D0');
+   PostLabel(L1);
+   EmitLn('MOVE D0,-(SP)');
+   Block(L2);
+   EmitLn('MOVE (SP)+,D0');
+   EmitLn('DBRA D0,' + L1);
+   EmitLn('SUBQ #2,SP');
+   PostLabel(L2);
+   EmitLn('ADDQ #2,SP');
+end;
+
+{--------------------------------------------------------------}
+{ Recognize and Translate a BREAK }
+
+procedure DoBreak(L: string);
+begin
+   Match('b');
+   EmitLn('BRA ' + L);
+end;
+
+{--------------------------------------------------------------}
+{ Recognize and Translate an "Other" }
+
+procedure Other;
+begin
+   EmitLn(GetName);
+end;
+
+{--------------------------------------------------------------}
+{ Recognize and Translate a Statement Block }
+
+procedure Block(L: string);
+begin
+   while not(Look in ['e', 'l', 'u']) do begin
+      case Look of
+       'i': DoIf(L);
+       'w': DoWhile;
+       'p': DoLoop;
+       'r': DoRepeat;
+       'f': DoFor;
+       'd': DoDo;
+       'b': DoBreak(L);
+       else Other;
+      end;
+   end;
+end;
+
+{--------------------------------------------------------------}
+{ Parse and Translate a Program }
+
+procedure DoProgram;
+begin
+   Block('');
+   if Look <> 'e' then Expected('End');
+   EmitLn('END')
+end;
+
+{--------------------------------------------------------------}
 { Initialize }
 
 procedure Init;
 begin
+   LCount := 0;
    GetChar;
-end;
-
-{ Forward declaration }
-procedure Expression; Forward;
-
-{---------------------------------------------------------------}
-{ Parse and Translate an Identifier }
-
-procedure Ident;
-var Name: char;
-begin
-   Name := GetName;
-   if Look = '(' then begin
-      Match('(');
-      Match(')');
-      EmitLn('BSR ' + Name);
-      end
-   else
-      EmitLn('MOVE ' + Name + '(PC),D0')
-end;
-
-{---------------------------------------------------------------}
-{ Parse and Translate a Math Factor }
-
-procedure Factor;
-begin
-   if Look = '(' then begin
-      Match('(');
-      Expression;
-      Match(')');
-      end
-   else if IsAlpha(Look) then
-      Ident
-   else
-      EmitLn('MOVE #' + GetNum + ',D0');
-end;
-
-{--------------------------------------------------------------}
-{ Recognize and Translate a Multiply }
-
-procedure Multiply;
-begin
-   Match('*');
-   Factor;
-   EmitLn('MULS (SP)+,D0');
-end;
-
-{-------------------------------------------------------------}
-{ Recognize and Translate a Divide }
-
-procedure Divide;
-begin
-   Match('/');
-   Factor;
-   EmitLn('MOVE (SP)+,D1');
-   EmitLn('DIVS D1,D0');
-end;
-
-{---------------------------------------------------------------}
-{ Parse and Translate a Math Term }
-
-procedure Term;
-begin
-   Factor;
-   while Look in ['*', '/'] do begin
-      EmitLn('MOVE D0,-(SP)');
-      case Look of
-       '*': Multiply;
-       '/': Divide;
-      else Expected('Mulop');
-      end;
-   end;
-end;
-
-
-{--------------------------------------------------------------}
-{ Recognize and Translate an Add }
-
-procedure Add;
-begin
-   Match('+');
-   Term;
-   EmitLn('ADD (SP)+,D0');
-end;
-
-{-------------------------------------------------------------}
-{ Recognize and Translate a Subtract }
-
-procedure Subtract;
-begin
-   Match('-');
-   Term;
-   EmitLn('SUB (SP)+,D0');
-   EmitLn('NEG D0');
-end;
-
-{---------------------------------------------------------------}
-{ Parse and Translate an Expression }
-
-procedure Expression;
-begin
-   if IsAddop(Look) then
-      EmitLn('CLR D0')
-   else
-      Term;
-   while IsAddop(Look) do begin
-      EmitLn('MOVE D0,-(SP)');
-      case Look of
-       '+': Add;
-       '-': Subtract;
-      else Expected('Addop');
-      end;
-   end;
-end;
-
-{--------------------------------------------------------------}
-{ Parse and Translate an Assignment Statement }
-
-procedure Assignment;
-var Name: char;
-begin
-   Name := GetName;
-   Match('=');
-   Expression;
-   EmitLn('LEA ' + Name + '(PC),A0');
-   EmitLn('MOVE D0,(A0)')
 end;
 
 {--------------------------------------------------------------}
@@ -264,7 +368,6 @@ end;
 
 begin
    Init;
-   Assignment;
-   if Look <> CR then Expected('Newline');
+   DoProgram;
 end.
 {--------------------------------------------------------------}
